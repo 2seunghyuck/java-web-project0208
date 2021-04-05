3c43fa9f8b5434efc5c4a38126aaed2d40c47ae9
2seugnhyuck <rotid1818@gmail.com>
2021-04-05 17:29:49 +0900

    python_day_05

--- python-basic/sutdyPyFile/python_day_05.py
+++ python-basic/sutdyPyFile/python_day_05.py
@@ -385,8 +385,75 @@ for i in range(10):
 print(result)
 
 
+# # BigData 를위한 각종 수학함수들
+
+# In[4]:
+
+
+import numpy as np
+import matplotlib.pylab as plt
+
+def sigmoid_function(x):
+    return 1/(1+np.exp(-x))
+
+x = np.linspace(-5,5)
+y = sigmoid_function(x)
+
+plt.plot(x, y)
+plt.show()
+
+
+# In[3]:
+
+
+def tanh_function(x):
+    return np.tanh(x)
+
+x = np.linspace(-5,5)
+y = tanh_function(x)
+
+plt.plot(x, y)
+plt.show()
+
+
+# In[6]:
+
+
+def relu_function(x):
+    return np.where(x<=0, 0, x)
+
+x = np.linspace(-5,5)
+y = relu_function(x)
+
+plt.plot(x, y)
+plt.show()
+
+
+# In[7]:
+
+
+def lecky_relu_function(x):
+    return np.where(x<=0, 0.01*x, x)
+
+x = np.linspace(-5,5)
+y = lecky_relu_function(x)
+
+plt.plot(x, y)
+plt.show()
+
+
+# In[8]:
+
+
+def softmax_function(x):
+    return np.exp(x)/np.sum(np.exp(x)) # 소프트맥스 함수
+
+y = softmax_function(np.array([1,2,3]))
+print(y)
+
+
 # In[ ]:
 
 
-result = []
+
 
